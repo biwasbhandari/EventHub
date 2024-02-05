a38afb4cad9ce11ea8ad109ad5df092594a2c19a
xbiwas <biwas2059@gmail.com>
2024-02-05 23:19:14 +0545

    final commit

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -1,66 +1,66 @@
-// "use client";
+"use client";
 
-// import { useCallback, Dispatch, SetStateAction } from "react";
-// import type { FileWithPath } from "@uploadthing/react";
-// import { useDropzone } from "@uploadthing/react/hooks";
-// import { generateClientDropzoneAccept } from "uploadthing/client";
+import { useCallback, Dispatch, SetStateAction } from "react";
 
-// import { Button } from "@/components/ui/button";
-// import { convertFileToUrl } from "@/lib/utils";
+import { useDropzone } from "@uploadthing/react/hooks";
+import { generateClientDropzoneAccept } from "uploadthing/client";
 
-// type FileUploaderProps = {
-//   onFieldChange: (url: string) => void;
-//   imageUrl: string;
-//   setFiles: Dispatch<SetStateAction<File[]>>;
-// };
+import { Button } from "@/components/ui/button";
+import { convertFileToUrl } from "@/lib/utils";
 
-// export function FileUploader({
-//   imageUrl,
-//   onFieldChange,
-//   setFiles,
-// }: FileUploaderProps) {
-//   const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
-//     setFiles(acceptedFiles);
-//     onFieldChange(convertFileToUrl(acceptedFiles[0]));
-//   }, []);
+type FileUploaderProps = {
+  onFieldChange: (url: string) => void;
+  imageUrl: string;
+  setFiles: Dispatch<SetStateAction<File[]>>;
+};
 
-//   const { getRootProps, getInputProps } = useDropzone({
-//     onDrop,
-//     accept: "image/*" ? generateClientDropzoneAccept(["image/*"]) : undefined,
-//   });
+export function FileUploader({
+  imageUrl,
+  onFieldChange,
+  setFiles,
+}: FileUploaderProps) {
+  const onDrop = useCallback((acceptedFiles: any[]) => {
+    setFiles(acceptedFiles);
+    onFieldChange(convertFileToUrl(acceptedFiles[0]));
+  }, []);
 
-//   return (
-//     <div
-//       {...getRootProps()}
-//       className="flex-center bg-dark-3 flex h-72 cursor-pointer flex-col overflow-hidden rounded-xl bg-grey-50"
-//     >
-//       <input {...getInputProps()} className="cursor-pointer" />
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: "image/*" ? generateClientDropzoneAccept(["image/*"]) : undefined,
+  });
 
-//       {imageUrl ? (
-//         <div className="flex h-full w-full flex-1 justify-center ">
-//           <img
-//             src={imageUrl}
-//             alt="image"
-//             width={250}
-//             height={250}
-//             className="w-full object-cover object-center"
-//           />
-//         </div>
-//       ) : (
-//         <div className="flex-center flex-col py-5 text-grey-500">
-//           <img
-//             src="/assets/icons/upload.svg"
-//             width={77}
-//             height={77}
-//             alt="file upload"
-//           />
-//           <h3 className="mb-2 mt-2">Drag photo here</h3>
-//           <p className="p-medium-12 mb-4">SVG, PNG, JPG</p>
-//           <Button type="button" className="rounded-full">
-//             Select from computer
-//           </Button>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
+  return (
+    <div
+      {...getRootProps()}
+      className="flex-center bg-dark-3 flex h-72 cursor-pointer flex-col overflow-hidden rounded-xl bg-grey-50"
+    >
+      <input {...getInputProps()} className="cursor-pointer" />
+
+      {imageUrl ? (
+        <div className="flex h-full w-full flex-1 justify-center ">
+          <img
+            src={imageUrl}
+            alt="image"
+            width={250}
+            height={250}
+            className="w-full object-cover object-center"
+          />
+        </div>
+      ) : (
+        <div className="flex-center flex-col py-5 text-grey-500">
+          <img
+            src="/assets/icons/upload.svg"
+            width={77}
+            height={77}
+            alt="file upload"
+          />
+          <h3 className="mb-2 mt-2">Drag photo here</h3>
+          <p className="p-medium-12 mb-4">SVG, PNG, JPG</p>
+          <Button type="button" className="rounded-full">
+            Select from computer
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+}
