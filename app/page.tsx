38203816fc26988b5426b58bf64fd25171b12deb
xbiwas <biwas2059@gmail.com>
2024-02-05 15:23:50 +0545

    updated

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Click Me</Button>
-    </main>
-  );
-}
