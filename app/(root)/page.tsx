48bf57eb8f9065550dbb120ed4680358c8218169
xbiwas <biwas2059@gmail.com>
2024-02-06 19:56:25 +0545

    typos fixed image optimized

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -41,7 +41,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
             alt="hero"
             width={1000}
             height={1000}
-            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh] rounded-lg"
           />
         </div>
       </section>
